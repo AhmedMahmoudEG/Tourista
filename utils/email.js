1710741e60cf9a413ce573840089c89ed3e1a3f6
Ahmad Mahmoud <ahmadfcis1998@gmail.com>
2025-10-09 16:15:11 +0300

    fixing

--- utils/email.js
+++ utils/email.js
@@ -1,51 +1,54 @@
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 const pug = require('pug');
 const htmlToText = require('html-to-text');
 const path = require('path');
 
 module.exports = class Email {
   constructor(user, url) {
-    ((this.to = user.email), (this.firstName = user.name.split(' ')[0]));
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Ahmad Mahmoud ${process.env.EMAIL_FROM}`;
+    this.from = `Ahmad Mahmoud <${process.env.EMAIL_FROM}>`;
+
+    // Set SendGrid API Key
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   }
-  newTransport() {
-    // This single transport configuration works for both development (Mailtrap)
-    // and production (SendGrid/Brevo) by reading credentials from the environment.
-    // This is more robust and easier to manage.
-    return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-  }
+
   async send(template, subject) {
-    //send the actual email
-    //1) render html for email based on pug template
+    // 1) Render HTML from your Pug template
     const html = pug.renderFile(
       path.join(__dirname, `../views/emails/${template}.pug`),
       { firstName: this.firstName, url: this.url, subject }
     );
-    //2) define email options
-    const mailOptions = {
-      from: this.from,
+
+    // 2) Define the email
+    const msg = {
       to: this.to,
+      from: this.from,
       subject,
       html,
       text: htmlToText.convert(html),
     };
-    //3) create a transport and send email
-    await this.newTransport().sendMail(mailOptions);
+
+    // 3) Send via SendGrid API
+    try {
+      await sgMail.send(msg);
+      console.log(`✅ Email sent to ${this.to}`);
+    } catch (err) {
+      console.error(`❌ Error sending email: ${err.message}`);
+      if (err.response) {
+        console.error(err.response.body);
+      }
+    }
   }
+
   async sendWelcome() {
     await this.send('welcome', 'Welcome to Tourista Family!');
   }
+
   async sendPasswordReset() {
     await this.send(
-      `passwordReset`,
+      'passwordReset',
       'Your password reset token (valid for only 10 minutes)'
     );
   }
